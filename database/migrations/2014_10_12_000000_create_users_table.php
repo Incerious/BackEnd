e11bd7bd71224fa14b02afbc847461c431da024d
Incerious <53209011+Incerious@users.noreply.github.com>
2019-07-29 14:25:48 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->bigIncrements('id')->autoIncrement();
             $table->string('username');
             $table->string('name');
+            $table->string('alamat');
+            $table->integer('no_hp');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
